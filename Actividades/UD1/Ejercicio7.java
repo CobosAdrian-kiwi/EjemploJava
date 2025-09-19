fd0334d73534a8802a62a83cf8ffc5ae36c5fd0b
CobosAdrian-kiwi <cobos.franco.adrian@iescamas.es>
2025-09-19 17:55:25 +0200

    Add files via upload

---
+++ Actividades/UD1/Ejercicio7.java
@@ -0,0 +1,23 @@
+package com;
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//Definimos las variables
+		double radioUsuario, longitud, area;
+		
+		//preguntamos por el radio deseado
+		System.out.println("CALCULADORA DE CIRCUNFERENCIAS: \n Introduce el radio: ");
+		radioUsuario = sc.nextDouble();
+		
+		//calculamos la longitud y el area
+		longitud = 2 * Math.PI * radioUsuario;
+		area = Math.PI * (radioUsuario * 2);
+		
+		//decimos el resultado
+		System.out.println("La longitud y el área de tu circunferencia son: \n Longitud: " + longitud + "\n Área: " + area);
+	}
+
+}
