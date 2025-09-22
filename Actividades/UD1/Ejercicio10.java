7a5c402f10794c97dd115ac2a33221450330f2be
CobosAdrian-kiwi <cobos.franco.adrian@iescamas.es>
2025-09-22 17:10:32 +0200

    Ejercicio10.java

--- Actividades/UD1/Ejercicio10.java
+++ Actividades/UD1/Ejercicio10.java
@@ -6,24 +6,27 @@ public class Ejercicio10 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		//defino las variables, str para las respuestas y las normales para definir booleanos
 		String llueveStr, biblioStr, tareaStr;
 		boolean lloviendo, biblioteca, tarea, puedeSalir;
 		
+		//pregunto si llueve
 		System.out.println("¿Está lloviendo (si/no)?");
 		llueveStr = sc.nextLine();
-		
-		lloviendo = llueveStr == "si";
+		//defino si es true o false en la otra variable
+		lloviendo = llueveStr.equals("si");
 		
 		System.out.println("¿Tienes tarea (si/no)?");
 		tareaStr = sc.nextLine();
 		
-		tarea = tareaStr == "si";
+		tarea = tareaStr.equals("si");
 		
 		System.out.println("¿Tienes que ir a la biblioteca (si/no)?");
 		biblioStr = sc.nextLine();
 		
-		biblioteca = biblioStr == "si";
+		biblioteca = biblioStr.equals("si");
 		
+		//comprobacion para ver si puede salir es true o false
 		puedeSalir = lloviendo == false && tarea == false || biblioteca == true;
 		
 		System.out.println("Puede salir? " + puedeSalir);
